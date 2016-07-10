d0cfcaf130365653287d123a26df7a37edc89066
craigthelinguist <aaroncraig@protonmail.ch>
2016-07-10 19:58:19 +1200

    Remove unused files

--- lib/linux/SDL/SDL_atomic.d
+++
@@ -1,8 +0,0 @@
-build/SDL_atomic.lo: \
- /home/craig/Desktop/SDL/SDL2-2.0.4/src/atomic/SDL_atomic.c \
- /home/craig/Desktop/SDL/SDL2-2.0.4/src/atomic/../SDL_internal.h \
- /home/craig/Desktop/SDL/SDL2-2.0.4/src/atomic/../dynapi/SDL_dynapi.h \
- /home/craig/Desktop/SDL/SDL2-2.0.4/src/atomic/../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h include/SDL_platform.h include/begin_code.h \
- include/close_code.h include/SDL_atomic.h include/SDL_stdinc.h \
- include/SDL_config.h
--- lib/linux/SDL/SDL_syspower.d
+++
@@ -1,8 +0,0 @@
-build/SDL_syspower.lo: \
- /home/craig/Desktop/SDL/SDL2-2.0.4/src/power/linux/SDL_syspower.c \
- /home/craig/Desktop/SDL/SDL2-2.0.4/src/power/linux/../../SDL_internal.h \
- /home/craig/Desktop/SDL/SDL2-2.0.4/src/power/linux/../../dynapi/SDL_dynapi.h \
- /home/craig/Desktop/SDL/SDL2-2.0.4/src/power/linux/../../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h include/SDL_platform.h include/begin_code.h \
- include/close_code.h include/SDL_power.h include/SDL_stdinc.h \
- include/SDL_config.h
