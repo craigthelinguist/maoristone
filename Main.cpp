142b6d9c1c3160979af49865c83d074ee4666576
craigthelinguist <aaroncraig@protonmail.ch>
2016-07-10 19:56:47 +1200

    Set up SDL

--- Main.cpp
+++ Main.cpp
@@ -1,26 +1,23 @@
-/** A mechanism for cleaner imports. **/
-#define QUOTEME(X) QUOTEME_1(X)
-#define QUOTEME_1(X) #X
-
-#ifdef _LINUX
-    #define LIBRARY(M) QUOTEME(SDL2/M)
-#elif defined(_WINDOWS)
-    #define LIBRARY(M) QUOTEME(lib\windows\M)
-#else
-    #error Error resolving import paths; your operating system is unknown.
-#endif
 
 /** Imports. **/
+#include "Modules.h"
 #include <iostream>
-#include LIBRARY(SDL.h)
 #include "MainGame.h"
+#include LIBRARY(SDL.h)
 
-int main(int argc, char** argv) {
-    MainGame mainGame;
-    mainGame.run();
+
+int main(int argc, char* argv[]) {
     
-    std::cout << "ITSUPGOGOOG!!";
-    system("pause");
-
+    // Initialise SDL.
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+	fprintf(stderr, "SDL could not initialise! SDL_Error: %s\n", SDL_GetError());
+	exit(EXIT_FAILURE);
+    }
+    
+    // Set up the game.
+    MainGame game(1280, 720);
+    game.init();
+    
     return 0;
+    
 }
--- MainGame.cpp
+++ MainGame.cpp
@@ -1,22 +1,41 @@
 #include "MainGame.h"
 
-MainGame::MainGame() {
+MainGame::MainGame(int screenWidth, int screenHeight) {
     _window = nullptr;
-    _screenWidth = 1280;
-    _screenHeight = 720;
+    _surface = nullptr;
+    _screenWidth = screenWidth;
+    _screenHeight = screenHeight;
+    
 }
 
 MainGame::~MainGame() {
     
 }
 
-void MainGame::run() {
-    initSystems();
-}
-
-
-void MainGame::initSystems() {
+void MainGame::init() {
+    
+    // Construct the window.
+    _window = SDL_CreateWindow("maoristone", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+			       _screenWidth, _screenHeight, SDL_WINDOW_OPENGL);
+    
+    // Construct the surface.
+    _surface = SDL_GetWindowSurface(_window);
+    SDL_FillRect(_surface, NULL, SDL_MapRGB(_surface->format, 0xFF, 0xFF, 0xFF));
+    SDL_UpdateWindowSurface(_window);
+    SDL_Delay(200);
+    
+    // 1280 x 720
+    
+    /*
     SDL_Init(SDL_INIT_EVERYTHING); //initialize everything from SDL
 
     _window = SDL_CreateWindow("maoristone", SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, _screenWidth, _screenHeight, SDL_WINDOW_OPENGL); 
+    */
 }
+
+
+void MainGame::run() {
+}
+
+
+
